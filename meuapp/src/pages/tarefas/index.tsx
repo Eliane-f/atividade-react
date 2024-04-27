a59298c67f1c21d1379acb393e382791b0da940c
Eliane <eliane.fabre@fatec.sp.gov.br>
2024-04-26 21:30:17 -0300

    aula 26/04

--- meuapp/src/pages/tarefas/index.tsx
+++ meuapp/src/pages/tarefas/index.tsx
@@ -1,28 +1,54 @@
-import Layout from "../../components/layout";
-import NovaTarefa from "../../components/nova-tarefa";
-import Tarefa from "../../components/tarefa";
+import { Link } from "react-router-dom"
+import Layout from "../../components/layout"
+import { useEffect, useState } from "react"
+import ItemTarefa from "../../components/tarefas/tarefa"
+import FormTarefa from "../../components/tarefas/form"
+import { Tarefa } from "../../interfaces/tarefa"
+
+function Tarefas(){
+
+    
+    const [tarefas, setTarefas] = useState<Tarefa[]>([])
+
+    function carregarLista(){
+        return [
+            {id: 1, nome: "Tarefa 1", concluida: false},
+            {id: 2, nome: "Tarefa 2", concluida: false}
+        ]
+    }
+
+    useEffect(() => {
+        alert('Render')
+        const tarefas = carregarLista()
+        setTarefas(tarefas)
+    },[])
 
 
-function Atividades(){
-    return(
+
+    function apagarTarefa(id: number){
+        const tarefasAtualizadas = tarefas.filter((tarefa) => tarefa.id !== id)
+        setTarefas(tarefasAtualizadas)
+    }
+    
+    return (
         <Layout>
-            <div style={{ marginTop: '40px' }}>
-                <h1>Lista de tarefas</h1>
-            </div>
-            <div style={{ marginTop: '20px' }}>
-                <hr /> {}
-                <h2 style={{fontWeight: 'bold'}}>Insirir nova tarefa</h2>
-                <NovaTarefa></NovaTarefa>
-                <hr /> {}
-                
-                <Tarefa tarefa='Tarefa 1' ></Tarefa>
-                <Tarefa tarefa='Tarefa 2' ></Tarefa>
-                <Tarefa tarefa='Tarefa 3' ></Tarefa>
-                <Tarefa tarefa='Tarefa 4' ></Tarefa>
-            </div>
+            <h1 style={{ marginTop: '40px' }}>Tarefas</h1>
+            <Link to='/'>Voltar para Home</Link>
+            <hr />
+            <FormTarefa 
+            tarefas={tarefas} 
+            setTarefas={setTarefas} />
+            {
+                tarefas.map((tarefa) => (
+                    <ItemTarefa
+                    key={tarefa.id}
+                    apagarTarefa={apagarTarefa} 
+                    titulo={tarefa.nome} 
+                    idTarefa={tarefa.id} />
+                ))
+            }
         </Layout>
     )
-    
 }
 
-export default Atividades
+export default Tarefas
